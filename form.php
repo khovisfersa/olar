2eadbf3f99a53ea02b8203d6e5d6d30a764c18e7
khovis <lucaskfsantos@gmail.com>
2017-02-06 05:02:17 -0300

    form.php ajeitado

--- form.php
+++ form.php
@@ -1,39 +1,28 @@
 <html>
-	<head>
-		<title>Esse é o titulo dessa bagaça, Coroi</title>
-	</head>
 	<body>
-    <table border = '1'>
-		  <thead>
-		  	<tr>
-		  		<th>elemento</th>
-		  		<th>dia da semana</th>
-		  		<th>prato do dia</th>
-		  		<th> preço </th>
-		  	</tr>
-		  </thead>
-  		<tbody>
-  			<?php 
-  				$diaDaSemana = ['domingo', 'segunda', 'terca' ,'quarta', 'quinta' , 'sexta', 'sabado'];
-  				$elemento = ['0', '1', '2', '3', '4', '5', '6'];
-  				$pratoDoDia = ['arroz', 'feijao', 'mandioca braba', 'rapadra com oregano', 'mato', 'little kill', 'sua mãe'];
-  				$preco = ['10', '20', '30', '40', '50', '60', 'gratis'];
+		<?php
+			$nome = $_POST['name'];
+			$endere = $_POST['ender'];
+			$idade = $_POST['idade'];
+			$sex = $_POST['sexo'];
+
+
+
+			if ($idade < 18 )
+				{
+					echo "Menor de idade";
+				}
+			else
+				{
 				
-
-  				foreach (range(0,6) as $elemento ) 
-  				{
-  					echo '<tr>' ;
-  					echo '<td>'.$elemento.'</td>';
-  					echo '<td>'.$diaDaSemana[$elemento].'</td>';
-  					echo '<td>'.$pratoDoDia[$elemento].'</td>';
-  					echo '<td>'.$preco[$elemento].'</td>';
-  					echo '</tr>';
-	  			}
-
-	  			echo "Hoje é ".$diaDaSemana[date('w')].". O prato de hoje é: ".$pratoDoDia[date('w')]." agorista, e custa míseros: ".$preco[date('w')];
-          echo "aceitamos bitcoin";
-	  		?>
-	  	</tbody>
-    </table>
+					echo $nome;
+					echo '<br>';
+					echo $endere;
+					echo '<br>';
+					echo $idade;
+					echo '<br>';
+					echo $sex;
+				}
+			?>
 	</body>
 </html>
